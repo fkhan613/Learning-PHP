14cb870c07640c0e5c98900e98963db8ccc47049
Farhan Khan <farhan.k2005@gmail.com>
2022-08-31 12:05:03 -0400

    learned about static

--- OOP_PHP/create_classes/includes/newClasses.php
+++ OOP_PHP/create_classes/includes/newClasses.php
@@ -19,7 +19,7 @@ abstract class User{
     }
 
 
-    //getters
+    //getters & setters
     public function getfName(){return $this->fName;}
     public function getlName(){return $this->lName;}
     public function getAge(){return $this->age;}
@@ -30,6 +30,7 @@ class Employee extends User{
 
     //property for employee
     private $role = "";
+    private static $race = "HUMAN";
 
     //constructor
     public function __construct($fName, $lName, $age, $role){ 
@@ -42,11 +43,41 @@ class Employee extends User{
         echo($this->__toString());
     }
 
-    //getter
+    //getter & getter
     public function getRole(){return $this->role;}
+    public static function getRace(){return self::$race;}
+    public static function setRace($race){self::$race = $race;}
 
     public function __toString(){
         return "<strong>EMPLOYEE</strong><br>ID: " . parent::getId() . 
-        "<br>First Name: " . parent:: getfName() . "<br>Last Name: " . parent:: getlName() . "<br>Role: " . $this->role . "<br><br>"; 
+        "<br>First Name: " . parent:: getfName() . "<br>Last Name: " . parent:: getlName() . "<br>Role: " . $this->role . "<br>Race: " . self::getRace() . "<br><br>"; 
+    }
+}
+
+class Alien extends User{
+
+    //property for employee
+    private $role = "";
+    private static $race = "Alien";
+
+    //constructor
+    public function __construct($fName, $lName, $age, $role){ 
+        parent::__construct($fName, $lName, $age);
+        $this->role = $role;
+    }
+
+    //destructor
+    public function __destruct(){
+        echo($this->__toString());
+    }
+
+    //getters & setters
+    public function getRole(){return $this->role;}
+    public static function getRace(){return self::$race;}
+    public static function setRace($race){self::$race = $race;}
+
+    public function __toString(){
+        return "<strong>ALIEN</strong><br>ID: " . parent::getId() . 
+        "<br>First Name: " . parent:: getfName() . "<br>Last Name: " . parent:: getlName() . "<br>Role: " . $this->role . "<br>Race: " . self::getRace() . "<br><br>"; 
     }
 }
--- OOP_PHP/create_classes/index.php
+++ OOP_PHP/create_classes/index.php
@@ -5,13 +5,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Creating Classes</title>
+    <title>OOP Practice</title>
 </head>
 <body>
     <?php 
         $John = new Employee("John", "Smith", 45, "Software Engineer");
         $James = new Employee("James", "Smith", 69, "Software Engineer II");
         $Aiden = new Employee("Aiden", "Smith", 15, "Full Stack Engineer");
+        $Yaba = new Alien("Yaba", "Doo", 99999, "Technical Areospace Engineer");
     ?>
 </body>
 </html>
